421472e7de249ab9a3539d66db64612dee3c0614
Mateusz <mateuszzawal557@gmail.com>
2024-04-25 21:01:06 +0200

    Kol01-2021 - step 2

--- Kolokwium01_2021/src/Country.java
+++ Kolokwium01_2021/src/Country.java
@@ -1,5 +1,12 @@
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public abstract class Country {
     private final String name;
+    private static Path confirmedCasesPath;
+    private static Path deathsPath;
 
     public String getName() {
         return name;
@@ -7,5 +14,35 @@ public abstract class Country {
 
     public Country(String name) {
         this.name = name;
+    }
+
+    public static void setFiles(Path deaths, Path confirmedCases) {
+        try {
+            if (!Files.isReadable(deaths)) {
+                throw new FileNotFoundException(deaths.toString());
+            }
+            if (!Files.isReadable(confirmedCases)) {
+                throw new FileNotFoundException(confirmedCases.toString());
+            }
+            deathsPath = deaths;
+            confirmedCasesPath = confirmedCases;
+        } catch (Exception e) {
+
+        }
+    }
+
+    public static Country fromCsv(String countryName) {
+        Country country = null;
+        try {
+            Scanner deathScanner = new Scanner(deathsPath);
+            Scanner casesScanner = new Scanner(confirmedCasesPath);
+
+            deathScanner.close();
+            casesScanner.close();
+        } catch (Exception e) {
+
+        }
+
+        return country;
     }
 }
