c4e73055f67da1c8f3ab44e3579e174d97d5872b
Mateusz <mateuszzawal557@gmail.com>
2024-04-25 21:58:50 +0200

    Kol01-2021 - step 4

--- Kolokwium01_2021/src/CountryWithoutProvinces.java
+++ Kolokwium01_2021/src/CountryWithoutProvinces.java
@@ -1,5 +1,19 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CountryWithoutProvinces extends Country{
+    List<LocalDate> dates = new ArrayList<>();
+    List<Integer> causesNumbers = new ArrayList<>();
+    List<Integer> deathsNumbers = new ArrayList<>();
+
     public CountryWithoutProvinces(String name) {
         super(name);
+    }
+
+    public void addDailyStatistic(LocalDate date, int causes, int deaths) {
+        dates.add(date);
+        causesNumbers.add(causes);
+        deathsNumbers.add(deaths);
     }
 }
