59f4f1c89703cc6f9a6a266f297ce457f01c5a78
Mateusz <mateuszzawal557@gmail.com>
2024-04-26 08:57:26 +0200

    Kol01-2023 - step 3

--- Kolokwium01_2023/src/Point.java
+++ Kolokwium01_2023/src/Point.java
@@ -2,6 +2,14 @@ public class Point {
     private final Double x;
     private final Double y;
 
+    public Double getX() {
+        return x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
     public Point(Double x, Double y) {
         this.x = x;
         this.y = y;
--- Kolokwium01_2023/src/Polygon.java
+++ Kolokwium01_2023/src/Polygon.java
@@ -7,4 +7,33 @@ public class Polygon {
     public Polygon(List<Point> points) {
         this.points = points;
     }
+
+    public boolean insidePoint(Point point){
+        int counter = 0;
+        List<Point> pointsCopy = points;
+        for(int i=0; i<pointsCopy.size()-1; i++) {
+            Point first = pointsCopy.get(i);
+            Point second = pointsCopy.get(i+1);
+            if(first.getY() > second.getY()) {
+                Point temp = first;
+                first = second;
+                second = temp;
+            }
+            if(first.getY() < point.getY() && point.getY() < second.getY()) {
+                double x;
+                double d = second.getX() - first.getX();
+                if(d == 0) {
+                    x = first.getX();
+                } else {
+                    double a = (second.getY() - first.getY()) / d;
+                    double b = first.getY() - a * first.getX();
+                    x = (point.getY() - b) / a;
+                }
+                if(x < point.getX()) {
+                    counter++;
+                }
+            }
+        }
+        return counter % 2 != 0;
+    }
 }
