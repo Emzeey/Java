474342127efab45f0f0f469abe3bc6239a82b125
Mateusz <mateuszzawal557@gmail.com>
2024-04-26 08:46:08 +0200

    Kol01-2023 - step 2

---
+++ Kolokwium01_2023/src/Polygon.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Polygon {
+    List<Point> points = new ArrayList<>();
+
+    public Polygon(List<Point> points) {
+        this.points = points;
+    }
+}
